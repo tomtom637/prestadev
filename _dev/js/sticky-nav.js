39c152a676d0eb67459a436be6e4a0a011afb520
Thomas Lacombe <tomtom637@gmail.com>
2021-10-22 02:49:00 +0200

    fixed a bug in the sticky header-top

--- _dev/js/sticky-nav.js
+++ _dev/js/sticky-nav.js
@@ -1,18 +1,20 @@
-const nav = document.querySelector(".header-top");
-let currentNavOffset = nav.scrollTop;
+document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.querySelector(".header-top");
+  let currentNavOffset = nav.offsetTop;
 
-function fixedNav() {
-  const scrollBody = document.body.scrollTop || document.documentElement.scrollTop;
-  const wrapper = document.querySelector("#wrapper");
-  if (scrollBody > currentNavOffset) {
-    nav.classList.add("fixed-nav");
-    if(window.getComputedStyle(nav).position === 'fixed') {
-      wrapper.style.marginTop = nav.offsetHeight + "px";
+  function fixedNav() {
+    const scrollBody = document.body.scrollTop || document.documentElement.scrollTop;
+    const wrapper = document.querySelector("#wrapper");
+    if (scrollBody > currentNavOffset) {
+      nav.classList.add("fixed-nav");
+      if(window.getComputedStyle(nav).position === 'fixed') {
+        wrapper.style.marginTop = nav.offsetHeight + "px";
+      }
+    } else {
+      wrapper.style.marginTop = "0";
+      nav.classList.remove("fixed-nav");
+      currentNavOffset = nav.offsetTop;
     }
-  } else {
-    wrapper.style.marginTop = "0";
-    nav.classList.remove("fixed-nav");
-    currentNavOffset = nav.offsetTop;
   }
-}
-window.onscroll = fixedNav;
+  window.onscroll = fixedNav;
+});
